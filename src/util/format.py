85040fd31d1ac80452b7146192c33ba72552bdc2
smollchungusm <mpamuller@msn.com>
2024-02-07 11:33:23 +0100

    EAGLE script formatting map

---
+++ src/util/format.py
@@ -0,0 +1,11 @@
+# Mapping of special characters to QMK labels
+special_char_mapping = {
+    '-': 'MINS',
+    '&': 'AMPR',
+    '*': 'ASTR',
+    '[': 'LBRC',
+    ']': 'RBRC',
+    ',': 'COMM',
+    '.': 'DOT'
+}
+
